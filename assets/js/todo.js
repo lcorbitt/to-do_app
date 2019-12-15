6f8c4b6c2fd7a12f013aa796b4e5025c755293ac
Lukas Corbitt <movalley5@gmail.com>
2019-12-15 16:27:30 -0700

    Added styles

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,23 +1,37 @@
 $(document).ready(function() {
-	// $('div, input, ul, span').css('border', '3px solid red');
-
 	$('ul').on('click', 'li', function() {
 		$(this).toggleClass('completed');
 	});
 
-	$('ul').on('click', 'span', function(event) {
+	$('ul').on('click', 'span', function(e) {
 		$(this).parent().fadeOut(500, function() {
 			$(this).remove();
 		});
-		event.stopPropagation();
+		e.stopPropagation();
 	});
 
 	$('#input').keypress(function(e) {
 		if (e.which === 13) {
 			var newText = $(this).val();
-			var newSpan = '<span>X </span>';
+			var newSpan = '<span><i class="fa fa-trash"></i></span> ';
 			$('ul').append('<li>' + newSpan + newText + '</li>');
 			$('#input').val('');
 		}
+	});
+
+	$('#deleteAll').on('click', function() {
+		deleteAll();
+	});
+
+	function deleteAll() {
+		$('li').fadeOut(500, function() {
+			$(this).remove();
+		});
+	}
+
+	$('.fa-minus').click(function() {
+		$(this).toggleClass('fa-minus');
+		$(this).toggleClass('fa-plus');
+		$('ul').fadeToggle();
 	});
 });
